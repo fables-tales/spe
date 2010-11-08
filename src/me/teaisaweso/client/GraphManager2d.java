51b513b7693aef8423cc5a4fa16af81d253335fc
Sam Phippen <samphippen@googlemail.com>
2010-11-08 22:10:15 +0800

    add a graphmanager2d interface
    this isn't quite done yet, it needs to have the output to canvas method
    added to it, but has most of the input stuff from the buttons complete
    
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

---
+++ src/me/teaisaweso/client/GraphManager2d.java
@@ -0,0 +1,76 @@
+package me.teaisaweso.client;
+
+import me.teaisaweso.shared.Graph;
+import me.teaisaweso.shared.Vertex;
+import me.teaisaweso.shared.VertexDirection;
+
+public interface GraphManager2d {
+
+    /**
+     * gets the underlying graph for this 2d representation
+     * 
+     * @return a new graph instance
+     */
+    public Graph getUnderlyingGraph();
+
+    /**
+     * adds a vertex to the graph
+     * 
+     * the vertex may not be centered at the passed location
+     * 
+     * @param v
+     *            the vertex to be added
+     * @param xPosition
+     *            the center x position of the vertex
+     * @param yPosition
+     *            the center y position of the vertex
+     */
+    public void addVertex(Vertex v, int xPosition, int yPosition, int size);
+
+    /**
+     * removes a vertex from the graph
+     * 
+     * @param v
+     *            the vertex to be removed
+     */
+    public void removeVertex(Vertex v);
+
+    /**
+     * moves a vertex to a given position
+     * 
+     * the vertex may not be centered at the passed location
+     * 
+     * @param v
+     *            the vertex to be moved
+     * @param xPosition
+     *            the new center x position of the vertex
+     * @param yPosition
+     *            the new center y position of the vertex
+     */
+    public void moveVertexTo(Vertex v, int xPosition, int yPosition);
+
+    /**
+     * changes the size of a vertex
+     * 
+     * may displace this or other vertices
+     * 
+     * @param v
+     *            the vertex to have it's size changed
+     * @param newSize
+     *            the new size of the vertex in pixels
+     */
+    public void scaleVertex(Vertex v, int newSize);
+
+    /**
+     * adds an edge between two vertices
+     * 
+     * @param v1
+     *            the "from" vertex
+     * @param v2
+     *            the "to" vertex
+     * @param dir
+     *            the edge direction
+     */
+    public void addEdge(Vertex v1, Vertex v2, VertexDirection dir);
+
+}
