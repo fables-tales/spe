fe7d58bd87b0e18e00963b21ffb7c041f67cfaa6
Sam Phippen <samphippen@googlemail.com>
2010-11-08 14:47:26 +0000

    reformat the edge class to look nicer
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- src/me/teaisaweso/shared/Edge.java
+++ src/me/teaisaweso/shared/Edge.java
@@ -7,34 +7,42 @@ public class Edge {
 
     /**
      * creates an edge
-     * @param v1 the "from" vertex
-     * @param v2 the "to" vertex
-     * @param dir the edge direction
+     * 
+     * @param v1
+     *            the "from" vertex
+     * @param v2
+     *            the "to" vertex
+     * @param dir
+     *            the edge direction
      */
     public Edge(Vertex v1, Vertex v2, VertexDirection dir) {
         mVertex1 = v1;
         mVertex2 = v2;
         mDir = dir;
     }
-    
+
     /**
      * determines if the edge exits a vertex
-     * @param v the vertex to test against
+     * 
+     * @param v
+     *            the vertex to test against
      * @return true if this edge exits v, else false
      */
     public boolean exits(Vertex v) {
         if (mDir == VertexDirection.both && (v == mVertex1 || v == mVertex2)) return true;
-        else return v ==  ((mDir == VertexDirection.fromTo) ? mVertex1 : mVertex2);
+        else return v == ((mDir == VertexDirection.fromTo) ? mVertex1 : mVertex2);
     }
-    
+
     /**
      * determines if the edge enters a vertex
-     * @param v the vertex to test against
+     * 
+     * @param v
+     *            the vertex to test against
      * @return true if this edge enters v, else false
      */
     public boolean enters(Vertex v) {
         if (mDir == VertexDirection.both && (v == mVertex1 || v == mVertex2)) return true;
-        else return v ==  ((mDir == VertexDirection.toFrom) ? mVertex1 : mVertex2);
+        else return v == ((mDir == VertexDirection.toFrom) ? mVertex1 : mVertex2);
     }
 
 }
