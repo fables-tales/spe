edaa429d319a42e84d8b38affd672333765e5303
Sam Phippen <samphippen@googlemail.com>
2010-11-08 22:40:24 +0800

    flesh out vertex a bit more
    now vertex can take a label on it's constructor and return it from a
    getter
    
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- src/me/teaisaweso/shared/Vertex.java
+++ src/me/teaisaweso/shared/Vertex.java
@@ -1,5 +1,21 @@
 package me.teaisaweso.shared;
 
 public class Vertex {
-	private String mString;
+	private String mLabel;
+	
+	/**
+	 * creates a vertex with a label
+	 * @param label
+	 */
+	public Vertex(String label) {
+	    mLabel = label;
+	}
+	
+	/**
+	 * gets the label of this vertex
+	 * @return
+	 */
+	public String getLabel() {
+	    return mLabel;
+	}
 }
