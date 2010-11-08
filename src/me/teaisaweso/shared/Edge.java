a0da2c4a7351ccb6a9d2cf0fa1173c0a7bbcfbbe
Sam Phippen <samphippen@googlemail.com>
2010-11-08 14:41:06 +0000

    flesh out edge a bit more
    now it's possible to determine whether an edge exits or enters a vertex
    
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- src/me/teaisaweso/shared/Edge.java
+++ src/me/teaisaweso/shared/Edge.java
@@ -1,13 +1,34 @@
 package me.teaisaweso.shared;
 
 public class Edge {
-	private Vertex mVertex1;
-	private Vertex mVertex2;
-	private VertexDirection mDir;
-	
-	public Edge(Vertex v1, Vertex v2, VertexDirection dir) {
-		mVertex1 = v1;
-		mVertex2 = v2;
-		mDir = dir;
-	}
+    private Vertex mVertex1;
+    private Vertex mVertex2;
+    private VertexDirection mDir;
+
+    public Edge(Vertex v1, Vertex v2, VertexDirection dir) {
+        mVertex1 = v1;
+        mVertex2 = v2;
+        mDir = dir;
+    }
+    
+    /**
+     * determines if the edge exits a vertex
+     * @param v the vertex to test against
+     * @return true if this edge exits v, else false
+     */
+    public boolean exits(Vertex v) {
+        if (mDir == VertexDirection.both && (v == mVertex1 || v == mVertex2)) return true;
+        else return v ==  ((mDir == VertexDirection.fromTo) ? mVertex1 : mVertex2);
+    }
+    
+    /**
+     * determines if the edge enters a vertex
+     * @param v the vertex to test against
+     * @return true if this edge enters v, else false
+     */
+    public boolean enters(Vertex v) {
+        if (mDir == VertexDirection.both && (v == mVertex1 || v == mVertex2)) return true;
+        else return v ==  ((mDir == VertexDirection.toFrom) ? mVertex1 : mVertex2);
+    }
+
 }
