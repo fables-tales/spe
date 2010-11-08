c2e22a4b12f3b8b2e4b94083e32826646c79a313
Sam Phippen <samphippen@googlemail.com>
2010-11-08 14:27:40 +0000

    add an edgedrawable class
    this represents an edge to be drawn by the drawing layer, including
    determining whether an arrow is needed
    
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

---
+++ src/me/teaisaweso/client/EdgeDrawable.java
@@ -0,0 +1,102 @@
+package me.teaisaweso.client;
+
+public class EdgeDrawable {
+    private int mStartX;
+    private int mStartY;
+
+    private int mEndX;
+    private int mEndY;
+
+    /**
+     * determines of the edge needs an arrow
+     * 
+     * arrows are always at the end edge
+     */
+    private boolean mArrow;
+
+    /**
+     * creates a new EdgeDrawable, with no arrow
+     * 
+     * @param startX
+     *            the start x of the edge
+     * @param startY
+     *            the start y of the edge
+     * @param endX
+     *            the end x of the edge
+     * @param endY
+     *            the end y of the edge
+     */
+    public EdgeDrawable(int startX, int startY, int endX, int endY) {
+        this(startX, startY, endX, endY, false);
+    }
+
+    /**
+     * creates a new EdgeDrawable
+     * 
+     * @param startX
+     *            the start x of the edge
+     * @param startY
+     *            the start y of the edge
+     * @param endX
+     *            the end x of the edge
+     * @param endY
+     *            the end y of the edge
+     * @param needsArrow
+     *            whether the end of the line needs an arrow
+     */
+    public EdgeDrawable(int startX, int startY, int endX, int endY, boolean needsArrow) {
+        mStartX = startX;
+        mStartY = startY;
+
+        mEndX = endX;
+        mEndY = endY;
+
+        mArrow = needsArrow;
+    }
+
+    /**
+     * gets the x of the start of the line
+     * 
+     * @return int representing x at the start of the line
+     */
+    public int getStartX() {
+        return mStartX;
+    }
+
+    /**
+     * gets the y of the start of the line
+     * 
+     * @return int representing y at the start of the line
+     */
+    public int getStartY() {
+        return mStartY;
+    }
+
+    /**
+     * gets the x of the end of the line
+     * 
+     * @return int represehnting x at the end of the line
+     */
+    public int getEndX() {
+        return mEndX;
+    }
+
+    /**
+     * gets the y at the end of the line
+     * 
+     * @return int representing y at the end of the line
+     */
+    public int getEndY() {
+        return mEndY;
+    }
+
+    /**
+     * determines if the line needs an arrow at the end
+     * 
+     * @return true if the line needs an arrow at the end else false
+     */
+    public boolean getNeedsArrow() {
+        return mArrow;
+    }
+
+}
