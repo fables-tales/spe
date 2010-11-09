e00cab2db18a6a6f81e09bcdbc90b9d72aeb0615
Sam Phippen <samphippen@googlemail.com>
2010-11-09 15:47:07 +0000

    add a blank GraphManager2d implementation class
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

---
+++ src/me/teaisaweso/client/graphmanagers/GraphManager2dImpl.java
@@ -0,0 +1,61 @@
+package me.teaisaweso.client.graphmanagers;
+
+import java.util.List;
+
+import me.teaisaweso.client.EdgeDrawable;
+import me.teaisaweso.client.VertexDrawable;
+import me.teaisaweso.shared.Graph;
+import me.teaisaweso.shared.Vertex;
+import me.teaisaweso.shared.VertexDirection;
+
+public class GraphManager2dImpl implements GraphManager2d {
+	
+	@Override
+	public Graph getUnderlyingGraph() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addVertex(Vertex v, int xPosition, int yPosition, int size) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void removeVertex(Vertex v) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void moveVertexTo(Vertex v, int xPosition, int yPosition) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void scaleVertex(Vertex v, int newSize) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addEdge(Vertex v1, Vertex v2, VertexDirection dir) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<VertexDrawable> getVertexDrawables() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<EdgeDrawable> getEdgeDrawables() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
