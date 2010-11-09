4309fd666c0ee2a408d7587f9f8ed2e461d9a014
Sam Phippen <samphippen@googlemail.com>
2010-11-09 15:36:19 +0000

    add a test for from-to edges
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- test/me/teaisaweso/shared/EdgeTest.java
+++ test/me/teaisaweso/shared/EdgeTest.java
@@ -41,5 +41,20 @@ public class EdgeTest extends TestCase {
 		Assert.assertEquals(false, e1.exits(mV1));
 	}
 
+	/**
+	 * tests a from-to edge has the right properties with an explicit
+	 * constructor
+	 */
+	public void testFromToEdgeExplicit() {
+		Edge e1 = new Edge(mV2, mV1, VertexDirection.fromTo);
+		Assert.assertEquals(true, e1.exits(mV2));
+		Assert.assertEquals(true, e1.enters(mV1));
+
+		Assert.assertEquals(false, e1.exits(mV1));
+		Assert.assertEquals(false, e1.enters(mV2));
+
+		Assert.assertEquals(false, e1.enters(mV3));
+		Assert.assertEquals(false, e1.exits(mV3));
+	}
 
 }
