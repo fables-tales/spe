a01b79bc753d8e113eea1cabb809f489eb9e2647
Sam Phippen <samphippen@googlemail.com>
2010-11-09 23:36:04 +0800

    reformat edgetest
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- test/me/teaisaweso/shared/EdgeTest.java
+++ test/me/teaisaweso/shared/EdgeTest.java
@@ -3,36 +3,43 @@ package me.teaisaweso.shared;
 import junit.framework.Assert;
 import junit.framework.TestCase;
 
-
-
 public class EdgeTest extends TestCase {
 
-    private Vertex mV1, mV2, mV3;
+	private Vertex mV1, mV2, mV3;
 
-    public void setUp() throws Exception {
-        mV1 = new Vertex("a");
-        mV2 = new Vertex("b");
-        mV3 = new Vertex("c");
-    }
+	public void setUp() throws Exception {
+		mV1 = new Vertex("a");
+		mV2 = new Vertex("b");
+		mV3 = new Vertex("c");
+	}
 
-    public void testBothEdgeImplicit() {
-        Edge e1 = new Edge(mV1, mV2);
-        Assert.assertEquals(true, e1.exits(mV1));
-        Assert.assertEquals(true, e1.enters(mV2));
-        Assert.assertEquals(true, e1.exits(mV2));
-        Assert.assertEquals(true, e1.enters(mV1));
-        Assert.assertEquals(false, e1.enters(mV3));
-        Assert.assertEquals(false, e1.exits(mV3));
-    }
-    
-    public void testBothEdgeExplicit() {
-        Edge e1 = new Edge(mV2, mV3, VertexDirection.both);
-        Assert.assertEquals(true, e1.exits(mV2));
-        Assert.assertEquals(true, e1.enters(mV3));
-        Assert.assertEquals(true, e1.exits(mV3));
-        Assert.assertEquals(true, e1.enters(mV2));
-        Assert.assertEquals(false, e1.enters(mV1));
-        Assert.assertEquals(false, e1.exits(mV1));
-    }
+	/**
+	 * tests that a both edge has the right properties with the implicit
+	 * constructor
+	 */
+	public void testBothEdgeImplicit() {
+		Edge e1 = new Edge(mV1, mV2);
+		Assert.assertEquals(true, e1.exits(mV1));
+		Assert.assertEquals(true, e1.enters(mV2));
+		Assert.assertEquals(true, e1.exits(mV2));
+		Assert.assertEquals(true, e1.enters(mV1));
+		Assert.assertEquals(false, e1.enters(mV3));
+		Assert.assertEquals(false, e1.exits(mV3));
+	}
+
+	/**
+	 * tests that a both edge has the right properties with an explict
+	 * constructor
+	 */
+	public void testBothEdgeExplicit() {
+		Edge e1 = new Edge(mV2, mV3, VertexDirection.both);
+		Assert.assertEquals(true, e1.exits(mV2));
+		Assert.assertEquals(true, e1.enters(mV3));
+		Assert.assertEquals(true, e1.exits(mV3));
+		Assert.assertEquals(true, e1.enters(mV2));
+		Assert.assertEquals(false, e1.enters(mV1));
+		Assert.assertEquals(false, e1.exits(mV1));
+	}
+
 
 }
