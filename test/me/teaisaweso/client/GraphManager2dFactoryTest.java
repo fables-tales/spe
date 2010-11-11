5cb01faff08f5c2b59e56528b8b922d33c26c1ae
Sam Phippen <samphippen@googlemail.com>
2010-11-11 23:10:48 +0800

    add a test for the graphmanager2d factory
    this is used to make sure the factory functions as it should, note that
    all of graphmanager will most likely break in the case where this class
    is broken
    
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

---
+++ test/me/teaisaweso/client/GraphManager2dFactoryTest.java
@@ -0,0 +1,26 @@
+package me.teaisaweso.client;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+import me.teaisaweso.client.graphmanagers.GraphManager2d;
+import me.teaisaweso.client.graphmanagers.GraphManager2dFactory;
+
+public class GraphManager2dFactoryTest extends TestCase {
+	/**
+	 * verifies that the factory produces a singleton
+	 */
+	public void testSingleton() {
+		GraphManager2dFactory f = GraphManager2dFactory.getInstance();
+		GraphManager2dFactory f2 = GraphManager2dFactory.getInstance();
+		// check that we only ever get one instance
+		Assert.assertEquals(true, f == f2);
+	}
+
+	/**
+	 * verifies that the factory product is not null
+	 */
+	public void testProduct() {
+		GraphManager2d manager = GraphManager2dFactory.getInstance().makeDefaultGraphManager();
+		Assert.assertNotNull(manager);
+	}
+}
