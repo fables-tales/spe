30bfba6fbbd74796ac6d6a9a26c7351403bebf0a
Sam Phippen <samphippen@googlemail.com>
2010-11-11 22:39:47 +0800

    format VertexDrawable.java
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- src/me/teaisaweso/client/VertexDrawable.java
+++ src/me/teaisaweso/client/VertexDrawable.java
@@ -1,77 +1,78 @@
 package me.teaisaweso.client;
 
 public class VertexDrawable {
-    private int mLeft;
-    private int mTop;
-    private int mWidth;
-    private int mHeight;
+	private int mLeft;
+	private int mTop;
+	private int mWidth;
+	private int mHeight;
 
-    private String mLabel;
+	private String mLabel;
 
-    /**
-     * create a new VertexDrawable
-     * 
-     * @param left
-     *            left of the bounding box
-     * @param top
-     *            top of the bounding box
-     * @param width
-     *            width of the bounding box
-     * @param height
-     *            height of the bounding box
-     * @param label
-     *            label of the vertex
-     */
-    public VertexDrawable(int left, int top, int width, int height, String label) {
-        this.mLeft = left;
-        this.mTop = top;
-        this.mWidth = width;
-        this.mHeight = height;
-        this.mLabel = label;
-    }
+	/**
+	 * create a new VertexDrawable
+	 * 
+	 * @param left
+	 *            left of the bounding box
+	 * @param top
+	 *            top of the bounding box
+	 * @param width
+	 *            width of the bounding box
+	 * @param height
+	 *            height of the bounding box
+	 * @param label
+	 *            label of the vertex
+	 */
+	public VertexDrawable(int left, int top, int width, int height, String label) {
+		this.mLeft = left;
+		this.mTop = top;
+		this.mWidth = width;
+		this.mHeight = height;
+		this.mLabel = label;
+	}
 
-    /**
-     * gets the left of the bounding box of the vertex
-     * 
-     * @return gets the left of the bounding box of the vertex
-     */
-    public int getLeft() {
-        return mLeft;
-    }
+	/**
+	 * gets the left of the bounding box of the vertex
+	 * 
+	 * @return gets the left of the bounding box of the vertex
+	 */
+	public int getLeft() {
+		return mLeft;
+	}
 
-    /**
-     * gets the top of the bounding box of the vertex
-     * 
-     * @return gets the top of the bounding box of the vertex
-     */
-    public int getTop() {
-        return mTop;
-    }
+	/**
+	 * gets the top of the bounding box of the vertex
+	 * 
+	 * @return gets the top of the bounding box of the vertex
+	 */
+	public int getTop() {
+		return mTop;
+	}
 
-    /**
-     * gets the width of the bounding box of the vertex
-     * 
-     * @return gets the width of the bounding box of the vertex
-     */
-    public int getWidth() {
-        return mWidth;
-    }
+	/**
+	 * gets the width of the bounding box of the vertex
+	 * 
+	 * @return gets the width of the bounding box of the vertex
+	 */
+	public int getWidth() {
+		return mWidth;
+	}
 
-    /**
-     * gets the height of the bounding box of the vertex
-     * 
-     * @return gets the height of the bounding box of the vertex
-     */
-    public int getHeight() {
-        return mHeight;
-    }
-    
-    /**
-     * gets the string label of this vertex
-     * @return a string representing the label of this vertex
-     */
-    public String getLabel() {
-        return mLabel;
-    }
+	/**
+	 * gets the height of the bounding box of the vertex
+	 * 
+	 * @return gets the height of the bounding box of the vertex
+	 */
+	public int getHeight() {
+		return mHeight;
+	}
+
+	/**
+	 * gets the string label of this vertex
+	 * 
+	 * @return a string representing the label of this vertex
+	 */
+	public String getLabel() {
+		return mLabel;
+	}
 
 }
