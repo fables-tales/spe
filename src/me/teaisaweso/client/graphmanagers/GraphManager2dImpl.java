b6be5e2d9245946dd25ca411a1b21a191d578631
Sam Phippen <samphippen@googlemail.com>
2010-11-11 22:59:06 +0800

    remove an import warning form GraphManager2d
    we weren't using linked list anywhere, so it doesn't need to be there
    
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- src/me/teaisaweso/client/graphmanagers/GraphManager2dImpl.java
+++ src/me/teaisaweso/client/graphmanagers/GraphManager2dImpl.java
@@ -3,7 +3,6 @@ package me.teaisaweso.client.graphmanagers;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
