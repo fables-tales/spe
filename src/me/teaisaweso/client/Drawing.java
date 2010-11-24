a3a745d20990a640be8bf86168810cc73877bbde
Max Robinson <max.s.robinson@gmail.com>
2010-11-24 10:47:49 +0000

    Fixed styling...again

--- src/me/teaisaweso/client/Drawing.java
+++ src/me/teaisaweso/client/Drawing.java
@@ -6,17 +6,17 @@ import com.google.gwt.widgetideas.graphics.client.GWTCanvas;
 
 public interface Drawing {
 
-	/**
-	 * adds a vertex to the graph
-	 * 
-	 * the vertex may not be centered at the passed location
-	 * 
-	 * @param canvas
-	 *            the name of the GWTCanvas to be rendered
-	 * @param edges
-	 *            the collection of vertices to be drawn
-	 * @param vertices
-	 *            the collection of edges to be drawn
-	 */
-	public void renderGraph (GWTCanvas canvas, Collection<EdgeDrawable> edges, Collection<VertexDrawable> vertices);
+    /**
+     * adds a vertex to the graph
+     * 
+     * the vertex may not be centered at the passed location
+     * 
+     * @param canvas
+     *            the name of the GWTCanvas to be rendered
+     * @param edges
+     *            the collection of vertices to be drawn
+     * @param vertices
+     *            the collection of edges to be drawn
+     */
+    public void renderGraph (GWTCanvas canvas, Collection<EdgeDrawable> edges, Collection<VertexDrawable> vertices);
 }
