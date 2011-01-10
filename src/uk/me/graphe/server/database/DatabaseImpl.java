60195f979f24c5e7e387af1d46e42b92fd276109
Max Robinson <max.s.robinson@gmail.com>
2011-01-10 15:49:17 +0000

    Methods implement but untested

--- src/uk/me/graphe/server/database/DatabaseImpl.java
+++ src/uk/me/graphe/server/database/DatabaseImpl.java
@@ -1,5 +1,7 @@
 package uk.me.graphe.server.database;
 
+import java.net.UnknownHostException;
+
 import uk.me.graphe.shared.Graph;
 
 import com.google.code.morphia.Datastore;
@@ -8,28 +10,35 @@ import com.mongodb.Mongo;
 
 public class DatabaseImpl implements Database{
     
+    private Mongo mMongo;
+    private Morphia mMorphia = new Morphia();
     private Datastore mData;
     
     public DatabaseImpl () {
-        
+        try {
+            mMongo = new Mongo();
+        } catch (UnknownHostException e) {
+            return;
+        }
+        mData = mMorphia.createDatastore(mMongo, "graphs");
     }
 
     @Override
     public void delete(int key) {
-        // TODO Auto-generated method stub
-        
+        mData.delete(DBItem.class, key);
     }
 
     @Override
     public Graph retrieve(int key) {
-        // TODO Auto-generated method stub
-        return null;
+        DBItem toRetrieve = mData.get(DBItem.class, key);
+        return toRetrieve.getGraph();
     }
 
     @Override
     public int store(Graph graph) {
-        // TODO Auto-generated method stub
-        return 0;
+        DBItem toStore = new DBItem(graph);
+        mData.save(toStore);
+        return toStore.getID();
     }
 
 }
