5c2f4a323c749c771d2c93e7766ec968a66b7778
Sam Phippen <samphippen@googlemail.com>
2011-01-11 13:00:41 +0000

    sort methods on graphprocessor and make it singleton
    we're never going to need mroe than one of this and it allows us to be fairly
    lazy about holding on to references of the thing.
    
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- src/uk/me/graphe/server/ot/GraphProcessor.java
+++ src/uk/me/graphe/server/ot/GraphProcessor.java
@@ -2,33 +2,32 @@ package uk.me.graphe.server.ot;
 
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
-import com.google.gwt.dev.util.Pair;
-
-
 
 import uk.me.graphe.graphmanagers.OTGraphManager2d;
 import uk.me.graphe.server.Client;
 import uk.me.graphe.server.ClientMessageSender;
 import uk.me.graphe.server.DataManager;
-import uk.me.graphe.server.messages.UpdateStateIdMessage;
+import uk.me.graphe.server.messages.StateIdMessage;
 import uk.me.graphe.server.messages.operations.CompositeOperation;
 import uk.me.graphe.server.messages.operations.GraphOperation;
 
-public class GraphProcessor extends Thread {
+import com.google.gwt.dev.util.Pair;
 
+public class GraphProcessor extends Thread {
+    
+    private static GraphProcessor sInstance = null;;
+    
+    public static GraphProcessor getInstance() {
+        if (sInstance == null) sInstance = new GraphProcessor();
+        return sInstance;        
+    }
+    
     private BlockingQueue<Pair<Client, GraphOperation>> mQueuedOperations = new ArrayBlockingQueue<Pair<Client, GraphOperation>>(
             1024);
-    
+
     private boolean mShutdown = false;
 
-    public void submit(Client c, GraphOperation o) {
-        Pair<Client, GraphOperation> pair = Pair.create(c, o);
-        try {
-            mQueuedOperations.put(pair);
-        } catch (InterruptedException e) {
-            throw new Error(e);
-        }
-    }
+    private GraphProcessor(){}
 
     @Override
     public void run() {
@@ -38,16 +37,31 @@ public class GraphProcessor extends Thread {
                 Client c = op.left;
                 GraphOperation operation = op.right;
                 int mGraphId = c.getCurrentGraphId();
+                
+                //get the graph and get the history delta
                 OTGraphManager2d graph = DataManager.getGraph(mGraphId);
                 CompositeOperation historyDelta = graph.getOperationDelta(operation.getHistoryId());
+                
+                //transform and apply the new operation
                 GraphOperation newOp = GraphTransform.transform(operation, historyDelta);
                 graph.applyOperation(newOp);
+                
+                //send update to client
                 int serverStateId = graph.getStateId();
-                ClientMessageSender.getInstance().sendMessage(c, new UpdateStateIdMessage(serverStateId));
+                ClientMessageSender.getInstance().sendMessage(c, new StateIdMessage(c.getCurrentGraphId(), serverStateId));
                 DataManager.save(graph);
             } catch (InterruptedException e) {
                 throw new Error(e);
             }
+        }
+    }
+    
+    public void submit(Client c, GraphOperation o) {
+        Pair<Client, GraphOperation> pair = Pair.create(c, o);
+        try {
+            mQueuedOperations.put(pair);
+        } catch (InterruptedException e) {
+            throw new Error(e);
         }
     }
 
