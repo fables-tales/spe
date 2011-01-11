ceb69b49f78af1cb93da374ed4f1141b310dca58
Sam Phippen <samphippen@googlemail.com>
2011-01-11 14:20:05 +0000

    make the message factory actually do things.
    In this case it instantiates message objects from json by finding their
    conversion factories and using them
    
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- src/uk/me/graphe/server/messages/MessageFactory.java
+++ src/uk/me/graphe/server/messages/MessageFactory.java
@@ -1,15 +1,36 @@
 package uk.me.graphe.server.messages;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import uk.me.graphe.server.messages.factories.AddEdgeFactory;
+import uk.me.graphe.server.messages.factories.AddNodeFactory;
+import uk.me.graphe.server.messages.factories.ConversionFactory;
+import uk.me.graphe.server.messages.factories.DeleteEdgeFactory;
+import uk.me.graphe.server.messages.factories.DeleteNodeFactory;
+import uk.me.graphe.server.messages.factories.HeartbeatFactory;
+import uk.me.graphe.server.messages.factories.MakeGraphFactory;
+import uk.me.graphe.server.messages.factories.MoveNodeFactory;
+
 public abstract class MessageFactory {
-    private static final Map<String, ConversionFactory> sOpFactoryMap = null;
+    private static Map<String, ConversionFactory> sOpFactoryMap = null;
     
+    static {
+        //TODO: get the strings from the classes instead of hardcode them
+        sOpFactoryMap = new HashMap<String, ConversionFactory>();
+        sOpFactoryMap.put("addEdge", new AddEdgeFactory());
+        sOpFactoryMap.put("delEdge", new DeleteEdgeFactory());
+        sOpFactoryMap.put("addNode", new AddNodeFactory());
+        sOpFactoryMap.put("delNode", new DeleteNodeFactory());
+        sOpFactoryMap.put("movNode", new MoveNodeFactory());
+        sOpFactoryMap.put("heartbeat", new HeartbeatFactory());
+        sOpFactoryMap.put("makeGraph", new MakeGraphFactory());
+    }
 
 	public static List<Message> makeOperationsFromJson(List<JSONObject> jsos) throws JSONException {
 		
