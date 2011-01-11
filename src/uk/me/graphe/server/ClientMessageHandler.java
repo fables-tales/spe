6c3915299adb68cdd545bf285cea6419d07ad7b5
Sam Phippen <samphippen@googlemail.com>
2011-01-11 16:54:06 +0000

    factoring of client message handler
    further broken down into methods, shutdown function added
    
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- src/uk/me/graphe/server/ClientMessageHandler.java
+++ src/uk/me/graphe/server/ClientMessageHandler.java
@@ -7,10 +7,14 @@ import java.util.Set;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import uk.me.graphe.graphmanagers.OTGraphManager2d;
 import uk.me.graphe.server.messages.Message;
 import uk.me.graphe.server.messages.MessageFactory;
+import uk.me.graphe.server.messages.NoSuchGraphMessage;
 import uk.me.graphe.server.messages.OpenGraphMessage;
+import uk.me.graphe.server.messages.RequestGraphMessage;
 import uk.me.graphe.server.messages.StateIdMessage;
+import uk.me.graphe.server.messages.operations.CompositeOperation;
 import uk.me.graphe.server.messages.operations.GraphOperation;
 import uk.me.graphe.server.ot.GraphProcessor;
 
@@ -64,30 +68,44 @@ public class ClientMessageHandler extends Thread {
         try {
             ops = MessageFactory.makeOperationsFromJson(jsos);
             for (Message message : ops) {
-                if (message.getMessage().equals("heartbeat")) {
-                    mHbm.beatWhenPossible(c);
-                } else if (message.getMessage().equals("makeGraph")) {
-                    int id = DataManager.create();
-                    c.setCurrentGraphId(id);
-                    ClientMessageSender.getInstance().sendMessage(
-                            c, new OpenGraphMessage(id));
-
-                    int stateId = DataManager.getGraph(id)
-                            .getStateId();
-                    ClientMessageSender.getInstance().sendMessage(
-                            c, new StateIdMessage(id, stateId));
-                } else if (message.isOperation()) {
-                    mProcessor.submit(c, (GraphOperation) message);
-                } else {
-                    throw new Error(
-                            "got unexpected message from client");
-                }
+                handleMessage(c, message);
             }
         } catch (JSONException e) {
             mClientManager.disconnect(c);
         } catch (InterruptedException e) {
-            mClientManager.disconnect(c);
-            throw new Error(e);
+            return;
+        }
+    }
+
+    private void handleMessage(Client c, Message message)
+            throws InterruptedException, Error {
+        if (message.getMessage().equals("heartbeat")) {
+            mHbm.beatWhenPossible(c);
+        } else if (message.getMessage().equals("makeGraph")) {
+            int id = DataManager.create();
+            c.setCurrentGraphId(id);
+            ClientMessageSender.getInstance().sendMessage(
+                    c, new OpenGraphMessage(id));
+
+            int stateId = DataManager.getGraph(id)
+                    .getStateId();
+            ClientMessageSender.getInstance().sendMessage(
+                    c, new StateIdMessage(id, stateId));
+        } else if (message.getMessage().equals("requestGraph")) {
+            System.err.println("got rgm");
+            RequestGraphMessage rgm = (RequestGraphMessage)message;
+            OTGraphManager2d g = DataManager.getGraph(rgm.getGraphId());
+            c.setCurrentGraphId(rgm.getGraphId());
+            if (g == null) ClientMessageSender.getInstance().sendMessage(c, new NoSuchGraphMessage());
+            else {
+                CompositeOperation delta = g.getOperationDelta(rgm.getSince());
+                ClientMessageSender.getInstance().sendMessage(c, delta);
+            }
+        } else if (message.isOperation()) {
+            mProcessor.submit(c, (GraphOperation) message);
+        } else {
+            throw new Error(
+                    "got unexpected message from client");
         }
     }
 
@@ -96,6 +114,7 @@ public class ClientMessageHandler extends Thread {
 
         for (String s : messages) {
             try {
+                System.err.println(s);
                 JSONObject o = new JSONObject(s);
                 result.add(o);
             } catch (JSONException e) {
@@ -111,6 +130,12 @@ public class ClientMessageHandler extends Thread {
     public static ClientMessageHandler getInstance() {
         if (sInstance == null) sInstance = new ClientMessageHandler();
         return sInstance;
+    }
+
+    public void shutDown() {
+        mShutDown = true;
+        this.interrupt();
+        mClientManager.wakeUp();
     }
 
 }
