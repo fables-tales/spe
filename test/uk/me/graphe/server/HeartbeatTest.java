ef3b62f348f81d65b8c62b0db1c614bc712be24e
Sam Phippen <samphippen@googlemail.com>
2011-01-11 14:39:23 +0000

    add a test for heartbeats
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

---
+++ test/uk/me/graphe/server/HeartbeatTest.java
@@ -0,0 +1,29 @@
+package uk.me.graphe.server;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+import uk.me.graphe.server.messages.HeartbeatMessage;
+import uk.me.graphe.server.messages.Message;
+
+public class HeartbeatTest extends TestCase {
+    
+    private TestClient mTestClient;
+    
+    @Override
+    public void setUp() {
+        mTestClient = new TestClient();
+        mTestClient.bringUpServer();
+        mTestClient.connect();
+    }
+    
+    public void testHeartbeat() {
+        mTestClient.sendMessage(new HeartbeatMessage());
+        Message m = mTestClient.readNextMessage();
+        Assert.assertEquals("heartbeat", m.getMessage());
+    }
+    
+    @Override
+    public void tearDown() {
+        mTestClient = null;
+    }
+}
