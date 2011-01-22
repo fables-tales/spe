c8c810291745de1b9cadd0fd64bf7ffdf6179db0
Ed Bevan <eb9374@bris.ac.uk>
2011-01-22 09:05:08 +0000

    Now has panning ability (drag mouse on an area that isn't a node while on move tool). Also whoever's job it is to do pictures could you find a better one for the move tool?

--- src/uk/me/graphe/client/Canvas.java
+++ src/uk/me/graphe/client/Canvas.java
@@ -3,6 +3,7 @@ package uk.me.graphe.client;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.MouseDownEvent;
 import com.google.gwt.event.dom.client.MouseMoveEvent;
+import com.google.gwt.event.dom.client.MouseOutEvent;
 import com.google.gwt.event.dom.client.MouseUpEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
@@ -31,32 +32,54 @@ public class Canvas extends Composite{
 	}
 	
 	public Graphemeui parent;
-	public int x1, x2, y1, y2;
+	public int x1, x2, y1, y2, panx, pany, offsetX, offsetY;
 	public boolean pressed;
 	@UiField
-	public CanvasWrapper canvasPanel;	
+	public CanvasWrapper canvasPanel;
 
 	public Canvas(Graphemeui parent) {
 		initWidget(uiBinder.createAndBindUi(this));
 		pressed = false;
+		offsetX = 0;
+		offsetY = 0;
 		this.parent = parent;
 	}
 	
 	@UiHandler("canvasPanel")
 	void onMouseDown(MouseDownEvent e){
+		//get initial click location
 		x1 = e.getX();
 		y1 = e.getY();
+		//make end point equal to start at beginning
+		x2 = x1;
+		y2 = y1;
+		//keeps track of previous end point for working out pan
+		panx = x1;
+		pany = y1;
+		//user is dragging until mouse up
 		pressed = true;
-		
 	}
 	
 	@UiHandler("canvasPanel")
 	void onMouseMove(MouseMoveEvent e){
+		//keep record of last end point
+		panx = x2;
+		pany = y2;
+		//get new end point
 		x2 = e.getX();
 		y2 = e.getY();
+		//if dragging using the move tool
 		if (parent.tools.getTool() == 5 && pressed) {
-			parent.move(x1, y1, x2, y2);
+			//call graphemeui move method
+			parent.move(x1-offsetX, y1-offsetY, panx-offsetX, pany-offsetY, x2-offsetX, y2-offsetY);
 		}
+	}
+	
+	@UiHandler("canvasPanel")
+	void onMouseOut(MouseOutEvent e){
+		pressed = false;
+		parent.moving = false;
+		parent.movingVertex = null;
 	}
 	
 	@UiHandler("canvasPanel")
@@ -68,7 +91,20 @@ public class Canvas extends Composite{
 			parent.tools.getOptionsPanel().remove(0);
 		}
 		if (parent.tools.getTool() == 1) {
-			parent.initOptions(x1, y1, x2, y2);
+			parent.initOptions(x1-offsetX, y1-offsetY, x2-offsetX, y2-offsetY);
 		}
+	}
+	
+	public void setOffset(int x, int y){
+		offsetX = x;
+		offsetY = y;
+	}
+	
+	public int getOffsetX(){
+		return offsetX;
+	}
+	
+	public int getOffsetY(){
+		return offsetY;
 	}
 }
