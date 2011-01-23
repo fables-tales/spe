3ed2e817f6689c7e2e508807c2222e95686a05bb
Max Robinson <max.s.robinson@gmail.com>
2011-01-23 19:57:49 +0000

    Added retrieve method

--- src/uk/me/graphe/server/database/DatabaseImpl.java
+++ src/uk/me/graphe/server/database/DatabaseImpl.java
@@ -2,7 +2,6 @@ package uk.me.graphe.server.database;
 
 import java.net.UnknownHostException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 
@@ -11,11 +10,15 @@ import uk.me.graphe.shared.Edge;
 import uk.me.graphe.shared.Vertex;
 import uk.me.graphe.shared.graphmanagers.OTGraphManager2d;
 import uk.me.graphe.shared.graphmanagers.OTGraphManager2dImpl;
+import uk.me.graphe.shared.messages.operations.AddEdgeOperation;
 import uk.me.graphe.shared.messages.operations.AddNodeOperation;
 import uk.me.graphe.shared.messages.operations.CompositeOperation;
+import uk.me.graphe.shared.messages.operations.DeleteEdgeOperation;
+import uk.me.graphe.shared.messages.operations.DeleteNodeOperation;
 import uk.me.graphe.shared.messages.operations.EdgeOperation;
 import uk.me.graphe.shared.messages.operations.GraphOperation;
 import uk.me.graphe.shared.messages.operations.MoveNodeOperation;
+import uk.me.graphe.shared.messages.operations.NoOperation;
 import uk.me.graphe.shared.messages.operations.NodeOperation;
 
 import com.google.code.morphia.Datastore;
@@ -45,12 +48,55 @@ public class DatabaseImpl implements Database{
 
     @Override
     public OTGraphManager2d retrieve(int key) {
+        //  Extract OtGraphManagerStore from DB
         List<OTGraphManager2dStore> retrieves = mData.find(OTGraphManager2dStore.class, "id=", key).asList();
         if (retrieves.size() != 1)
             throw new Error("Could not locate item");
         OTGraphManager2dStore retrieve = retrieves.get(0);
+        List<GraphDB> operations = retrieve.getmOps();
+        // Create new 2d Graph manager
         OTGraphManager2d toReturn = new OTGraphManager2dImpl(retrieve.getId());
         toReturn.setStateId(retrieve.getStateid());
+        List<GraphOperation> graphOperations = new ArrayList<GraphOperation>();
+        
+        for (GraphDB item : operations) {
+            GraphOperation toAdd = null;
+            int itemId = item.getHistoryId();
+            if (item.isEdgeOperation()) {
+                EdgeDB edgeOp = item.asEdgeOperation();
+                DBEdge edge = edgeOp.getEdge();
+                Edge thisEdge = new Edge(new Vertex(edge.getFrom().getLabel()), new Vertex(edge.getTo().getLabel()), edge.getDir());
+                if (edgeOp.createsEdge(edge)) {
+                    toAdd = new AddEdgeOperation(thisEdge);
+                    }
+                if (edgeOp.deletesEdge(edge)) {
+                    toAdd = new DeleteEdgeOperation(thisEdge);
+                }
+            }
+            if (item.isNodeOperation()) {
+                NodeDB nodeOp = item.asNodeOperation();
+                DBVertex vertex = nodeOp.getNode();
+                Vertex thisVertex = new Vertex(vertex.getLabel());
+                if (nodeOp.createsNode(vertex)) {
+                    AddNodeDB addOp = (AddNodeDB) nodeOp;
+                    toAdd = new AddNodeOperation(thisVertex, addOp.getX(), addOp.getY());
+                }        
+                if(nodeOp.deletesNode(vertex)) {
+                    toAdd = new DeleteNodeOperation(thisVertex);
+                }
+                    
+                if(nodeOp.movesNode(vertex)) {
+                    MoveNodeDB moveOp = (MoveNodeDB) nodeOp;
+                    toAdd = new MoveNodeOperation(thisVertex, moveOp.getToX(), moveOp.getToY());
+                }
+            }
+            if (item.isNoOperation()) {
+                toAdd = new NoOperation();
+            }
+            toAdd.setHistoryId(itemId);
+            graphOperations.add(toAdd);
+        }
+        toReturn.setHistory(graphOperations);
         return toReturn;
     }
 
@@ -101,7 +147,5 @@ public class DatabaseImpl implements Database{
         mData.save(toStore);
         return toStore.getId();
     }
-
-    
 
 }
