5ec366d2322e4694831002da68ff6960f95628a2
Max Robinson <max.s.robinson@gmail.com>
2011-01-23 11:20:25 +0000

    Added store method for OT2dGraphManager

--- src/uk/me/graphe/server/database/DatabaseImpl.java
+++ src/uk/me/graphe/server/database/DatabaseImpl.java
@@ -1,19 +1,21 @@
 package uk.me.graphe.server.database;
 
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 
-import uk.me.graphe.server.database.dbitems.GraphDB;
-import uk.me.graphe.server.database.dbitems.OTGraphManager2dStore;
+import uk.me.graphe.server.database.dbitems.*;
 import uk.me.graphe.shared.Edge;
 import uk.me.graphe.shared.Vertex;
 import uk.me.graphe.shared.graphmanagers.OTGraphManager2d;
 import uk.me.graphe.shared.graphmanagers.OTGraphManager2dImpl;
+import uk.me.graphe.shared.messages.operations.AddNodeOperation;
 import uk.me.graphe.shared.messages.operations.CompositeOperation;
 import uk.me.graphe.shared.messages.operations.EdgeOperation;
 import uk.me.graphe.shared.messages.operations.GraphOperation;
+import uk.me.graphe.shared.messages.operations.MoveNodeOperation;
 import uk.me.graphe.shared.messages.operations.NodeOperation;
 
 import com.google.code.morphia.Datastore;
@@ -55,32 +57,47 @@ public class DatabaseImpl implements Database{
     @Override
     public int store(OTGraphManager2d manager) {
         OTGraphManager2dStore toStore = new OTGraphManager2dStore(manager);
+        List<GraphDB> storedOperations = new ArrayList<GraphDB>();
         CompositeOperation history = manager.getCompleteHistory();
         List<GraphOperation> operations = history.asIndividualOperations();
         for (GraphOperation item : operations) {
+            GraphDB toAdd = null;
+            int itemId = item.getHistoryId();
             if (item.isEdgeOperation()) {
                 EdgeOperation edgeOp = item.asEdgeOperation();
                 Edge edge = edgeOp.getEdge();
-                if (edgeOp.createsEdge(edge))
-                    // Create DBEAddEdge
-                if (edgeOp.deletesEdge(edge))
-                    // Create DBDeleteEdge
-                    ;
+                DBEdge storeEdge = new DBEdge(edge, new DBVertex(edge.getFromVertex()), new DBVertex(edge.getToVertex()));
+                if (edgeOp.createsEdge(edge)) {
+                    toAdd = new AddEdgeDB(storeEdge);
+                    }
+                if (edgeOp.deletesEdge(edge)) {
+                    toAdd = new DeleteEdgeDB(storeEdge);
+                }
             }
             if (item.isNodeOperation()) {
                 NodeOperation nodeOp = item.asNodeOperation();
                 Vertex vertex = nodeOp.getNode();
-                if (nodeOp.createsNode(vertex))
+                DBVertex storeVertex = new DBVertex(vertex);
+                if (nodeOp.createsNode(vertex)) {
+                    AddNodeOperation addOp = (AddNodeOperation) nodeOp;
+                    toAdd = new AddNodeDB(storeVertex, addOp.getX(), addOp.getY());
+                }        
+                if(nodeOp.deletesNode(vertex)) {
+                    toAdd = new DeleteNodeDB(storeVertex);
+                }
                     
-                if(nodeOp.deletesNode(vertex))
-                    
-                if(nodeOp.movesNode(vertex))
-                    ;
+                if(nodeOp.movesNode(vertex)) {
+                    MoveNodeOperation moveOp = (MoveNodeOperation) nodeOp;
+                    toAdd = new MoveNodeDB(storeVertex, moveOp.getToX(), moveOp.getToY());
+                }
             }
             if (item.isNoOperation()) {
-                
+                toAdd = new NoOpDB();
             }
+            toAdd.setHistoryId(itemId);
+            storedOperations.add(toAdd);
         }
+        toStore.setmOps(storedOperations);
         mData.save(toStore);
         return toStore.getId();
     }
