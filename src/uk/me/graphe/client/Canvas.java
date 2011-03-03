6d0b3259a6cc294f35511da876aa091507a039d8
Ben Barron <forums@benbarron.com>
2011-03-03 15:26:40 +0000

    Implemented more features for toolbox.

--- src/uk/me/graphe/client/Canvas.java
+++ src/uk/me/graphe/client/Canvas.java
@@ -139,8 +139,8 @@ public class Canvas extends Composite{
 	@UiHandler("canvasPanel")
 	void onMouseOut(MouseOutEvent e){
 		isMouseDown = false;
-		parent.moving = false;
-		parent.movingVertex = null;
+		//parent.moving = false;
+		//parent.movingVertex = null;
 	}
 	
 	@UiHandler("canvasPanel")
@@ -164,8 +164,8 @@ public class Canvas extends Composite{
 				break;
 		}
 		//parent.tools.setTool(Tools.nameVertex);
-		parent.moving = false;
-		parent.movingVertex = null;
+		//parent.moving = false;
+		//parent.movingVertex = null;
 		isMouseDown = false;
 		/*if (parent.tools.getOptionsPanel().getWidgetCount() != 0) {
 			parent.tools.getOptionsPanel().remove(0);
