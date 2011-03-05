ec140d4aee5a710f365fa28465d4042aac13c8cb
Max Robinson <max.s.robinson@gmail.com>
2011-03-05 10:20:40 +0000

    Added methods to track sent status

--- src/uk/me/graphe/client/LocalStore.java
+++ src/uk/me/graphe/client/LocalStore.java
@@ -16,7 +16,8 @@ public class LocalStore {
         if (Storage.isSupported() == false )
             Window.alert("Warning your broswer does not support local storage, permanent high speed internet connection will be required to play");
         mStorage = Storage.getLocalStorage();
-        mStorage.setItem("itemID", "none");
+        if (mStorage.getItem("itemID") == null)
+        	mStorage.setItem("itemID", "none");
     }
     
     public void store (GraphOperation op){
@@ -31,6 +32,40 @@ public class LocalStore {
             return true;
         mStorage.clear();
         mStorage.setItem("itemID", id);
+        mStorage.setItem("unAcked", "");
+        mStorage.setItem("unSent", "");
+        mStorage.setItem("Sent", "");
         return false;
     }
+    
+    public void toUnSent(GraphOperation o) {
+    	int historyID = o.getHistoryId();
+        String histID = Integer.toString(historyID);
+        String unSent = mStorage.getItem("unSent");
+        mStorage.setItem("unSent", unSent.concat(" " + histID));
+    }
+    
+    public void toUnAck(GraphOperation o) {
+        int historyID = o.getHistoryId();
+        String histID = Integer.toString(historyID);
+        String unAcked = mStorage.getItem("unAcked");
+        String unSent = mStorage.getItem("unSent");
+        if (unSent.contains(histID))
+            mStorage.setItem("unSent", unSent.replace(" " + histID, ""));
+        mStorage.setItem("unAcked", unAcked.concat(" " + histID));
+    }
+    
+    public void toSent(GraphOperation o) {
+        int historyID = o.getHistoryId();
+        String histID = Integer.toString(historyID);
+        String unAcked = mStorage.getItem("unAcked");
+        String unSent = mStorage.getItem("unSent");
+        String Sent = mStorage.getItem("Sent");
+        if (unAcked.contains(histID))
+        	mStorage.setItem("unAcked", unAcked.replace(" "+ histID, ""));
+        if (unSent.contains(histID))
+            mStorage.setItem("unSent", unSent.replace(" " + histID, ""));
+        mStorage.setItem("Sent", Sent.concat(" " + histID));
+    }
+    
 }
