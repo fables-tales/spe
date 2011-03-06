eeb79185e9d96e9d8f5d3cee351a1606d9898dea
Max Robinson <max.s.robinson@gmail.com>
2011-03-06 17:11:26 +0000

    Added method to store the state of the graph into the localstore

--- src/uk/me/graphe/client/LocalStore.java
+++ src/uk/me/graphe/client/LocalStore.java
@@ -1,5 +1,6 @@
 package uk.me.graphe.client;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.StringTokenizer;
@@ -11,11 +12,11 @@ import com.google.gwt.user.client.Window;
 
 public class LocalStore {
     
+	int maxHistoryId = 0;
     Storage mStorage;
-    HashMap<Integer, GraphOperation> mOps = new HashMap<Integer, GraphOperation>();
     private enum State {Sent, Unsent, Unacked};
     HashMap<Integer, State> mStates = new HashMap<Integer, State>();
-    
+    HashMap<Integer, GraphOperation> mOps = new HashMap<Integer, GraphOperation>();
     
     public LocalStore() {
         if (Storage.isSupported() == false )
@@ -24,32 +25,42 @@ public class LocalStore {
         if (mStorage.getItem("itemID") == null)
         	mStorage.setItem("itemID", "none");
     }
-    
-    private GraphOperation retrieve () {
-    	String strOp = mStorage.getItem("");
-    	//List<GraphOperation> op = MessageFactory.
-    	
-    	return null;
-    }
-    
-    private void save (GraphOperation op){
-        String jsonOp = op.toJson();
-        String historyId= Integer.toString(op.getHistoryId());
-        mStorage.setItem(historyId, jsonOp);
-    }
-    
-    public void store (GraphOperation op) {
-    	mOps.put(Integer.valueOf(op.getHistoryId()),op);
+
+    public void store(GraphOperation op) {
+    	int id = op.getHistoryId();
+    	mOps.put(Integer.valueOf(id),op);
+    	if (id > maxHistoryId)
+    		maxHistoryId = id;
     	toUnsent(op);
     }
     
-    public void store (GraphOperation op, boolean Acked) {
-    	mOps.put(Integer.valueOf(op.getHistoryId()),op);
+    public void store(GraphOperation op, boolean Acked) {
+    	int id = op.getHistoryId();
+    	mOps.put(Integer.valueOf(id),op);
+    	if (id > maxHistoryId)
+    		maxHistoryId = id; 
     	if (Acked == true)
     		toSent(op);
 		else
 			toUnack(op);
     	
+    }
+    
+    public StorePackage getInformation() {
+    	List<GraphOperation> sent = new ArrayList<GraphOperation>();
+    	List<GraphOperation> unsent = new ArrayList<GraphOperation>();
+    	List<GraphOperation> unacked = new ArrayList<GraphOperation>();
+    	for (int i = 0; i < maxHistoryId; i++) {
+    		Integer id = Integer.valueOf(i);
+			State curState = mStates.get(id);
+			if (curState == State.Sent)
+				sent.add(mOps.get(id));
+			else if (curState == State.Unacked)
+				unacked.add(mOps.get(id));
+			else if (curState == State.Unsent)
+				unsent.add(mOps.get(id));
+    	}
+    	return new StorePackage(sent, unsent, unacked);
     }
     
     private boolean setgraph(int graphId) {
@@ -61,15 +72,23 @@ public class LocalStore {
         mStorage.setItem("unAcked", "");
         mStorage.setItem("unSent", "");
         mStorage.setItem("Sent", "");
+        
         return false;
     }
     
-    public void saveState() {
+    public void save() {
+    	String maxID = Integer.toString(maxHistoryId);
+    	mStorage.setItem("maxHistory", maxID);
+    	saveState();
+    	saveOps();
+    }
+    
+    private void saveState() {
     	int i;
     	String sent = "";
     	String unacked = "";
     	String unsent = "";
-    	for (i = 0; i < mStates.size(); i++) {
+    	for (i = 0; i < maxHistoryId; i++) {
 			State curState = mStates.get(Integer.valueOf(i));
 			if (curState == State.Sent)
 				sent = sent.concat(Integer.toString(i) + " ");
@@ -83,7 +102,17 @@ public class LocalStore {
 		mStorage.setItem("Sent", sent);
     }
     
-    public void restoreState() {
+    private void saveOps(){
+    	for (int i = 0; i < maxHistoryId; i++) {
+    		Integer id = Integer.valueOf(i);
+    		GraphOperation op = mOps.get(id);
+    		String jsonOp = op.toJson();
+    		String historyId= id.toString();
+    		mStorage.setItem(historyId, jsonOp);
+    	}
+    }
+    
+    private void restoreState() {
     	String sent = mStorage.getItem("Sent");
     	String unsent = mStorage.getItem("Unsent");
     	String unacked = mStorage.getItem("Unacked");
@@ -106,34 +135,16 @@ public class LocalStore {
     public void toUnsent(GraphOperation o) {
     	int historyID = o.getHistoryId();
     	mStates.put(historyID, State.Unsent);
-//        String histID = Integer.toString(historyID);
-//        String unSent = mStorage.getItem("unSent");
-//        mStorage.setItem("unSent", unSent.concat(" " + histID));
     }
     
     public void toUnack(GraphOperation o) {
         int historyID = o.getHistoryId();
         mStates.put(historyID, State.Unacked);
-//        String histID = Integer.toString(historyID);
-//        String unAcked = mStorage.getItem("unAcked");
-//        String unSent = mStorage.getItem("unSent");
-//        if (unSent.contains(histID))
-//            mStorage.setItem("unSent", unSent.replace(" " + histID, ""));
-//        mStorage.setItem("unAcked", unAcked.concat(" " + histID));
     }
     
     public void toSent(GraphOperation o) {
         int historyID = o.getHistoryId();
         mStates.put(historyID, State.Sent);
-//        String histID = Integer.toString(historyID);
-//        String unAcked = mStorage.getItem("unAcked");
-//        String unSent = mStorage.getItem("unSent");
-//        String Sent = mStorage.getItem("Sent");
-//        if (unAcked.contains(histID))
-//        	mStorage.setItem("unAcked", unAcked.replace(" "+ histID, ""));
-//        if (unSent.contains(histID))
-//            mStorage.setItem("unSent", unSent.replace(" " + histID, ""));
-//        mStorage.setItem("Sent", Sent.concat(" " + histID));
     }
     
 }
