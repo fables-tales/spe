f29929cb4508a9a7ca726fb93025d035d596e222
Max Robinson <max.s.robinson@gmail.com>
2011-03-07 13:48:17 +0000

    Removed redundant import

--- src/uk/me/graphe/client/LocalStoreImpl.java
+++ src/uk/me/graphe/client/LocalStoreImpl.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.StringTokenizer;
 
 import uk.me.graphe.shared.jsonwrapper.JSONException;
 import uk.me.graphe.shared.jsonwrapper.JSONImplHolder;
