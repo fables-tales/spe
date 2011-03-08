223bf8cc31baf7a317cceaae5db309c133edbce2
Max Robinson <max.s.robinson@gmail.com>
2011-03-08 20:16:00 +0000

    Rewrote factory to always return same instance

--- src/uk/me/graphe/client/LocalStoreFactory.java
+++ src/uk/me/graphe/client/LocalStoreFactory.java
@@ -2,7 +2,8 @@ package uk.me.graphe.client;
 
 public class LocalStoreFactory {
 
+    static LocalStoreImpl instance = new LocalStoreImpl();
 	public static LocalStore newInstance() {
-		return new LocalStoreImpl();
+		return instance;
 	}
 }
