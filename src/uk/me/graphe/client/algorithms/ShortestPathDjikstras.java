a94a8d9bda30350031b8cf1e8e3f4ec8a19033b1
Sam Phippen <samphippen@googlemail.com>
2011-04-13 12:04:05 +0100

    add a blank algorithm interface  and a blank djikstras implementation
    this is done so that we can clearly understand what needs to occur to
    implement algorithms
    
    this can also be hooked up to the drawing layer easily
    
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

---
+++ src/uk/me/graphe/client/algorithms/ShortestPathDjikstras.java
@@ -0,0 +1,47 @@
+package uk.me.graphe.client.algorithms;
+
+import java.util.List;
+
+import uk.me.graphe.shared.Edge;
+import uk.me.graphe.shared.Graph;
+import uk.me.graphe.shared.Vertex;
+
+public class ShortestPathDjikstras implements Algorithm {
+
+    @Override
+    public List<Edge> getHilightedEdges() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<Vertex> getHilightedVerticies() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getResult() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void initialise(Graph g) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void step() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void stepAll() {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
