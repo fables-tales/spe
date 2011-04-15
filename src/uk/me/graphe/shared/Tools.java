b1fb0cb10b930bbbd7eba2728e38dc840f541a5f
Ben Barron <forums@benbarron.com>
2011-04-15 22:23:52 +0100

    Added further tools for styling of nodes.

--- src/uk/me/graphe/shared/Tools.java
+++ src/uk/me/graphe/shared/Tools.java
@@ -1,5 +1,5 @@
 package uk.me.graphe.shared;
 
 public enum Tools{
-	addEdge, addVertex, autolayout, cluster, delete, move, nameVertex, weightEdge, select, zoom, process, decision, terminator
+	addEdge, addVertex, autolayout, cluster, delete, move, nameVertex, weightEdge, select, zoom, styleProcess, styleDecision, styleTerminator, styleNormal
 }
