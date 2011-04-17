0eb6fd27eb78a568f59c6a8177b5c061ce0ccfc2
Ben Barron <forums@benbarron.com>
2011-04-17 20:12:14 +0100

    Removed tools that aren't used anymore.

--- src/uk/me/graphe/shared/Tools.java
+++ src/uk/me/graphe/shared/Tools.java
@@ -1,5 +1,5 @@
 package uk.me.graphe.shared;
 
 public enum Tools{
-	addEdge, addVertex, autolayout, cluster, delete, move, nameVertex, weightEdge, select, zoom, styleProcess, styleDecision, styleTerminator, styleNormal
+	addEdge, addVertex, autolayout, cluster, delete, move, select, zoom, styleProcess, styleDecision, styleTerminator, styleNormal
 }
