e2e80688f609ba30b4b390524f0f2c7e11880698
Ben Barron <forums@benbarron.com>
2011-04-23 18:36:31 +0100

    Set style buttons to actually do something.

--- src/uk/me/graphe/client/Toolbox.java
+++ src/uk/me/graphe/client/Toolbox.java
@@ -110,19 +110,19 @@ public class Toolbox extends Composite {
 				this.setTool(Tools.select);
 				break;
 			case styleProcess:
-				// TODO: Implement
+				parent.setSelectedSyle(VertexDrawable.STROKED_SQUARE_STYLE);
 				this.setTool(Tools.select);
 				break;
 			case styleTerminator:
-				// TODO: Implement
+				parent.setSelectedSyle(VertexDrawable.STROKED_TERM_STYLE);
 				this.setTool(Tools.select);
 				break;
 			case styleDecision:
-				// TODO: Implement
+				parent.setSelectedSyle(VertexDrawable.STROKED_DIAMOND_STYLE);
 				this.setTool(Tools.select);
 				break;
 			case styleNormal:
-				// TODO: Implement
+				parent.setSelectedSyle(VertexDrawable.FILLED_CIRCLE_STYLE);
 				this.setTool(Tools.select);
 				break;
 		}
