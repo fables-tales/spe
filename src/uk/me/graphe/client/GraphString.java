f0d09757fe884eec8bc34a3c383604b161d4671b
Michael Jones <phyros@gmail.com>
2011-04-25 19:30:03 +0100

    Change output graph type to DOT

--- src/uk/me/graphe/client/GraphString.java
+++ src/uk/me/graphe/client/GraphString.java
@@ -4,8 +4,9 @@ import uk.me.graphe.shared.Graph;
 public class GraphString{ 
 
    
-    public static String getString(Graph graph){
-        return "New Graph";
+    public static String getDot(Graph graph){
+        String graphString = "graph ethane {C_0 -- H_0 [type=s];C_0 -- H_1 [type=s];}";
+        return graphString;
     }
     
 }
