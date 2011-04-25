367c0dd1400a6b952cefd84554035ef1dfd1e682
Michael Jones <phyros@gmail.com>
2011-04-25 19:07:11 +0100

    Class rename

---
+++ src/uk/me/graphe/client/GraphString.java
@@ -0,0 +1,11 @@
+package uk.me.graphe.client;
+
+import uk.me.graphe.shared.Graph;
+public class GraphString{ 
+
+   
+    public static String getString(Graph graph){
+        return "New Graph";
+    }
+    
+}
