768d964fd0427dd59d34e72a4d6519a85482b087
Sam Phippen <samphippen@googlemail.com>
2011-05-05 12:56:48 +0100

    make database test actually test some things
    it now tests that the values that come out of the database are
    actually the ones that we are expecting from what we've stored
    
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- test/uk/me/graphe/server/DatabaseTest.java
+++ test/uk/me/graphe/server/DatabaseTest.java
@@ -1,21 +1,23 @@
 package uk.me.graphe.server;
 
+import junit.framework.Assert;
 import junit.framework.TestCase;
 import uk.me.graphe.server.database.Database;
 import uk.me.graphe.server.database.DatabaseFactory;
 import uk.me.graphe.shared.Edge;
 import uk.me.graphe.shared.Vertex;
+import uk.me.graphe.shared.graphmanagers.GraphManager2d;
 import uk.me.graphe.shared.graphmanagers.OTGraphManager2d;
 import uk.me.graphe.shared.graphmanagers.OTGraphManagerFactory;
 import uk.me.graphe.shared.messages.operations.AddEdgeOperation;
 import uk.me.graphe.shared.messages.operations.AddNodeOperation;
 
-
-public class DatabaseTest extends TestCase{
+public class DatabaseTest extends TestCase {
 
     private Database db = DatabaseFactory.newInstance();
     private OTGraphManager2d mManager;
     private int graphId = 0;
+
     @Override
     protected void setUp() throws Exception {
         // TODO Auto-generated method stub
@@ -29,18 +31,23 @@ public class DatabaseTest extends TestCase{
         super.tearDown();
         mManager = null;
     }
-    
-    public void testStore () {
+
+    public void testStore() {
         Vertex v1 = new Vertex("v1");
         Vertex v2 = new Vertex("v2");
         mManager.applyOperation(new AddNodeOperation(v1, 1, 1));
         mManager.applyOperation(new AddNodeOperation(v2, 1, 4));
-        mManager.applyOperation(new AddEdgeOperation(new Edge(v1,v2)));
-        graphId = db.store(mManager);   
+        mManager.applyOperation(new AddEdgeOperation(new Edge(v1, v2)));
+        graphId = db.store(mManager);
+        GraphManager2d m = db.retrieve(graphId);
+        Assert.assertNotNull(m.getEdgeDrawables());        
+        Assert.assertEquals(mManager.getEdgeDrawables().size(), m.getEdgeDrawables().size());
+        Assert.assertNotNull(m.getVertexDrawables());
+        Assert.assertEquals(mManager.getVertexDrawables().size(), m.getVertexDrawables().size());
     }
-    
+
     public void testRetrieve() {
-        //OTGraphManager2d retrieve = db.retrieve(graphId);
-        
+        // OTGraphManager2d retrieve = db.retrieve(graphId);
+
     }
 }
