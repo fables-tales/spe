59747737c17f99fbbc1bcb82b080c031af93d14e
j0hnn <john.rpm@gmail.com>
2011-05-07 16:15:18 +0100

    fix graph list factory

--- src/uk/me/graphe/shared/messages/factories/GraphListFactory.java
+++ src/uk/me/graphe/shared/messages/factories/GraphListFactory.java
@@ -3,7 +3,7 @@ package uk.me.graphe.shared.messages.factories;
 import uk.me.graphe.shared.jsonwrapper.JSONException;
 import uk.me.graphe.shared.jsonwrapper.JSONObject;
 import uk.me.graphe.shared.messages.Message;
-import uk.me.graphe.shared.messages.UserAuthMessage;
+import uk.me.graphe.shared.messages.GraphListMessage;
 
 public class GraphListFactory implements ConversionFactory{
 
@@ -11,12 +11,9 @@ public class GraphListFactory implements ConversionFactory{
 	    public Message make(JSONObject o) {
 
 	        try {
-	            String opUrl = o.getString("opUrl");
-	            String reUrl = o.getString("reUrl");
-	            String emailAdd = o.getString("emailAdd");
-	            String openIdUrl = o.getString("openIdUrl");
-	            String authKey = o.getString("authKey");
-	            return new UserAuthMessage(opUrl, reUrl, emailAdd, openIdUrl, authKey);
+	            String list = o.getString("list");
+
+	            return new GraphListMessage(list);
 	        } catch (JSONException e) {
 	            throw new Error(e);
 	        }
