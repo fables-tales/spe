73b90304a9bd05714dc5528455c76ffd2bbf2132
Max Robinson <max.s.robinson@gmail.com>
2011-05-08 07:47:01 +0100

    Fixed DataManger to retrieve graphId = 1 from database if it exists before creating a new one

--- src/uk/me/graphe/server/DataManager.java
+++ src/uk/me/graphe/server/DataManager.java
@@ -23,9 +23,10 @@ public class DataManager {
     
     static {
         newMap();
-        create();
         mTimer.scheduleAtFixedRate(new Backup(), 120000, 120000);
         sHighestId = mDatabase.size();
+        if (sHighestId == 0)
+            create();
     }
 
     private static void newMap() {
